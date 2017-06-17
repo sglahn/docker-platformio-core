0ed9e216c69f5d4983be5335eff52565483edf46
Sebastian Glahn <hi@sgla.hn>
2017-06-17 21:39:51 +0200

    Externalized config folder

--- platformio.sh
+++ platformio.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-IMAGE_NAME=sglahn/platformio-core:3.3.1
+IMAGE_NAME=571710e11420
+#IMAGE_NAME=sglahn/platformio-core:3.3.1
 
 docker run --rm \
     -v `pwd`:/workspace \
+    -v "$HOME"/.platformio:/.platformio \
     -u `id -u $USER`:`id -g $USER` \
     --device=/dev/ttyUSB0 \
     $IMAGE_NAME \
